f64b266f93b633fd6a563111d42a39d9c92e6054
Firdews Abrar <88767436+kibehar@users.noreply.github.com>
2021-09-06 22:34:30 +0300

    Update project.final.cpp

--- project.final.cpp
+++ project.final.cpp
@@ -10,7 +10,6 @@ For doctor it is possible to enter tip and other patient requirement and to keep
 #include<iomanip>// used to construct table with proper width.
 #include<cmath>//used to calculation pary
 #include<cstdlib>//used for using setw which is for size of space
-#include<stdio.h>
 
 using namespace std;
 
@@ -389,7 +388,7 @@ cout<<"\nAge :                   "<<setw (32)<< left<<p[i].age;
 cout<<"\nSex :                   "<<setw (32)<< left<<p[i].sex;
 cout<<"\nBlood Group :           "<<setw (32)<< left<<p[i].blood;
 cout<<"\nBMI :                  "<<setw (30)<<left<<p[i+c].bmi;
-cout<<"\Medicine taken:         "<<setw (30)<< left<<p[i].medicine_taken<<endl;
+cout<<"\nMedicine taken:         "<<setw (30)<< left<<p[i].medicine_taken<<endl;
 cout<<"Number of medicine:     "<<setw (30)<< left<<p[i].no_medicine<<endl;
 cout<<"Name of illnesses:      "<<setw (30)<< left<<p[i].illnesses<<endl;
 cout<<"Type of surgeries:      "<<setw (30)<< left<<p[i].surgeries<<endl;
@@ -743,8 +742,8 @@ Hos.hos_add={"Addiss Ababa","Kolfe Keranyo",05,1,"birhanu_centrhos@gmail.com","0
 
 cout<<"Name of Hospital: "<<Hos.name<<endl;
 cout<<"\n->Service that "<<Hos.name<<" provide: "<<endl<<Hos.service<<endl;
-cout<<"\n General information:"<<"\n ->President — Peter L. Slavin,\n-> Total certified beds — 907\n-> Total employees (part- and full-time) — 23,302\n-> Total operating revenue — $612 million"<<endl;
-cout<<"\nPatient and surgical statistics: "<<"\n #Total inpatients — 47,250\n #Average length of stay — 5.82 days\n #Admissions to observe — 7,978\n #Total surgical cases — 36,701\n #Inpatient surgical cases — 19,233\n #Ambulatory surgical cases — 17,468"<<endl;
+cout<<"\n General information:"<<"\n ->President Â— Peter L. Slavin,\n-> Total certified beds Â— 907\n-> Total employees (part- and full-time) Â— 23,302\n-> Total operating revenue Â— $612 million"<<endl;
+cout<<"\nPatient and surgical statistics: "<<"\n #Total inpatients Â— 47,250\n #Average length of stay Â— 5.82 days\n #Admissions to observe Â— 7,978\n #Total surgical cases Â— 36,701\n #Inpatient surgical cases Â— 19,233\n #Ambulatory surgical cases Â— 17,468"<<endl;
 cout<<"\nDoctors information in "<<Hos.name<<endl;
 cout<<"______________________________________________________"<<endl;
 
